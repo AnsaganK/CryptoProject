f2abcb8c7f8b5f6fd6f03b7cedc8a068f8b97697
Ansagan-Kabdolla <kabdolla00.00@mail.ru>
2021-10-15 14:44:37 +0600

    fix bittrex api

--- crypto/urls.py
+++ crypto/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('bittrex/<str:pair>', views.get_bittrex, name='get_bittrex')
 ]
--- crypto/views.py
+++ crypto/views.py
@@ -1,5 +1,14 @@
+import requests
+from django.http import JsonResponse
 from django.shortcuts import render
 
 
 def index(request):
     return render(request, 'crypto/index.html')
+
+
+def get_bittrex(request, pair):
+    response = requests.get(f'https://api.bittrex.com/v3/markets/{pair}/ticker')
+    if response.status_code == 200:
+        return JsonResponse(response.json())
+    return {'lastTradeRate': '0'}
